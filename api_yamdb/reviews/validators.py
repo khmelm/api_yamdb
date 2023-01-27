90626c55ee18aed7c3d713850387873c35e407b1
khmelm <85014106+khmelm@users.noreply.github.com>
2023-01-27 10:21:33 +0300

    Update validators.py

--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -2,8 +2,11 @@ from django.utils import timezone
 
 from django.core.exceptions import ValidationError
 
+
 def validate_year(year):
     if year > timezone.now().year:
-        raise ValidationError({ 
+        raise ValidationError({
             'year': 'Год выпуска не может быть больше текущего года!'
-})
+            }
+        )
+
